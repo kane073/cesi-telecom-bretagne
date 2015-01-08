8d34f3f1fac11f841154a2d391b34c7011737cba
alassane.kane7 <alassane.kane7@4c6fab8b-6a8c-2ee4-6dcd-3da9ab1b7203>
2015-01-08 14:58:09 +0000

    Problème de généralition des id réglé, Service de création d'un institution de rattachement mise en place

---
+++ trunk/trunk/CallingCESI/src/eu/telecom_bretagne/CESI/demo/AppelGestionInstitution.java
@@ -0,0 +1,70 @@
+package eu.telecom_bretagne.CESI.demo;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import eu.telecom_bretagne.CESI.data.model.Institutionrattchement;
+import eu.telecom_bretagne.CESI.service.IGestionInstitution;
+
+public class AppelGestionInstitution {
+
+	public static void main(String[] args) {
+		test_listeInstitutions();
+		test_creerInstitution();
+		test_listeInstitutions();
+	}
+
+	public static IGestionInstitution getGestionInstitution() {
+		InitialContext ctx = null;
+		IGestionInstitution gestionInstitution = null;
+		try {
+			ctx = new InitialContext();
+			gestionInstitution = (IGestionInstitution) ctx
+					.lookup(IGestionInstitution.JNDI_NAME);
+		} catch (NamingException e) {
+			// Unable to retrieve the context or the service
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		return gestionInstitution;
+	}
+
+	public static void test_listeInstitutions() {
+		for (Institutionrattchement institutionrattchement : getGestionInstitution()
+				.listeInstitution()) {
+			afficherInstitution(institutionrattchement);
+		}
+	}
+
+	public static void test_lireInstitution() {
+		test_lireInstitution(1);
+	}
+
+	public static void test_lireInstitution(Integer id) {
+		Integer identifiant = id;
+		afficherInstitution(getGestionInstitution().lireInstitutionrattchement(
+				identifiant));
+	}
+
+	public static void test_creerInstitution() {
+		String nom = "Télécom Paris";
+		String adresse = "Rue Barrau";
+
+		Institutionrattchement institutionrattchement = getGestionInstitution().creerInstitution(nom, adresse);
+		if(null != institutionrattchement){
+			test_lireInstitution(institutionrattchement.getIdInstitution());
+		}else{
+			System.out.println("L'institution de rattachement existe déjà!");
+		}
+		
+
+	}
+
+	private static void afficherInstitution(
+			Institutionrattchement institutionrattchement) {
+		System.out.println("Nom institution :"
+				+ institutionrattchement.getNominstitution() + " Adresse : "
+				+ institutionrattchement.getAdresse() + "\n");
+	}
+
+}
