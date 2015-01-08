9aa8a0c643fe94f65864ceba6e110206fff8ab63
alassane.kane7 <alassane.kane7@4c6fab8b-6a8c-2ee4-6dcd-3da9ab1b7203>
2015-01-08 17:02:45 +0000

    Mise en place du service création d'un auteur

--- trunk/trunk/CESI_EJB_client/src/eu/telecom_bretagne/CESI/service/IGestionAuteur.java
+++ trunk/trunk/CESI_EJB_client/src/eu/telecom_bretagne/CESI/service/IGestionAuteur.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import eu.telecom_bretagne.CESI.data.model.Auteur;
+import eu.telecom_bretagne.CESI.data.model.Auteurexterne;
+import eu.telecom_bretagne.CESI.data.model.Auteurinterne;
 
 @Remote
 public interface IGestionAuteur {
@@ -12,4 +14,15 @@ public interface IGestionAuteur {
 	public static final String JNDI_NAME = "java:global/CESI_EAR/CESI_EJB/GestionAuteur!eu.telecom_bretagne.CESI.service.IGestionAuteur";
 	
 	public List<Auteur> listeAuteur();
+
+	public Auteur creerAuteur(String nom, String type, String prenom, String email,
+			String sitePerso, String idExterne, String idInterne,
+			int idInstitution);
+	
+
+	public Auteur creerAuteurExterne(String nom, String prenom, String email,
+			String sitePerso, String idExterne, int identifiantInstitut);
+
+	public Auteur creerAuteurInterne(String nom, String prenom, String email,
+			String sitePerso, String idInterne);
 }
